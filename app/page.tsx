aacce928d803be459974f9912a1a335fe8c96889
justshev <mshevaatallah@gmail.com>
2025-06-21 22:19:02 +0700

    seperate page file

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,9 @@
-"use client";
-
-import Aurora from "@/components/Aurora";
-import SplitText from "@/components/SplitText";
+import Hero from "@/sections/home/Hero";
 // import Aurora from "@/components/Aurora";
 export default function Home() {
-  const handleAnimationComplete = () => {
-    console.log("All letters have animated!");
-  };
   return (
     <div>
-      <Aurora colorStops={["#643d8e", "#000000", "#ffffff"]} />
-      <SplitText
-        text="Hello, GSAP!"
-        className="text-2xl font-semibold text-center"
-        delay={100}
-        duration={0.6}
-        ease="power3.out"
-        splitType="chars"
-        from={{ opacity: 0, y: 40 }}
-        to={{ opacity: 1, y: 0 }}
-        threshold={0.1}
-        rootMargin="-100px"
-        textAlign="center"
-        onLetterAnimationComplete={handleAnimationComplete}
-      />
+      <Hero />
     </div>
   );
 }

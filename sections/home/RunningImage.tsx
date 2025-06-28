41d2602c7e78e87c99671f304f523803cbb09ac1
justshev <mshevaatallah@gmail.com>
2025-06-28 18:12:29 +0700

    revisi responsive , masih ada zzz

--- sections/home/RunningImage.tsx
+++ sections/home/RunningImage.tsx
@@ -2,7 +2,7 @@ import PartnetSlider from "@/components/PartnerSlider";
 
 export default function RunningImage() {
   return (
-    <div className="flex justify-center items-center mt-30 mb-10 flex-col">
+    <div className="flex justify-center items-center mt-30 mb-10 flex-col sliderr">
       <div className="running-title">Work partnership</div>
       <div className="running-images-container mt-10">
         <PartnetSlider />

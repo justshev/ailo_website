6a232709731e4e96248e7548fbaa046174b874e0
justshev <mshevaatallah@gmail.com>
2025-06-28 08:30:04 +0700

    fix responsive

--- sections/home/Hero.tsx
+++ sections/home/Hero.tsx
@@ -6,40 +6,37 @@ export default function Hero() {
     <div>
       <Aurora colorStops={["#643d8e", "#000000", "#4079ff"]} />
       <div className="hero-text-content mt-10 flex justify-center items-center flex-col text-center">
-        <AnimatedOnScroll>
-          <p>
-            Total work and stuff up to 100+ projects.{" "}
-            <span className="ml-2 mr-1">Read more </span> <span> &#8594; </span>
-          </p>
-        </AnimatedOnScroll>
-        <AnimatedOnScroll>
-          <div className="main-title-hero ">
-            <span>Best AI</span>
-            <GradientText
-              colors={["#b80d00", "#4079ff", "#ffffff", "#4079ff", "#b80d00"]}
-              showBorder={false}
-              className="custom-class ml-2 mr-2"
-            >
-              Learning
-            </GradientText>
-            <span>&</span>
-            <GradientText
-              colors={["#b80d00", "#4079ff", "#ffffff", "#4079ff", "#b80d00"]}
-              showBorder={false}
-              className="custom-class ml-2 mr-2"
-            >
-              Optimization
-            </GradientText>
+        <p>
+          Total work and stuff up to 100+ projects.{" "}
+          <span className="ml-2 mr-1">Read more </span> <span> &#8594; </span>
+        </p>
 
-            <span>Center</span>
-          </div>
-        </AnimatedOnScroll>
-        <AnimatedOnScroll>
-          <p className="text-lg text-gray-400 mt-4 sec-title-hero">
-            AILO is Center of Excellence from Telkom University that focused on
-            AI Research and Development
-          </p>
-        </AnimatedOnScroll>
+        <div className="main-title-hero ">
+          <span>Best AI</span>
+          <GradientText
+            colors={["#b80d00", "#4079ff", "#ffffff", "#4079ff", "#b80d00"]}
+            showBorder={false}
+            className="custom-class ml-2 mr-2"
+          >
+            Learning
+          </GradientText>
+          <span>&</span>
+          <GradientText
+            colors={["#b80d00", "#4079ff", "#ffffff", "#4079ff", "#b80d00"]}
+            showBorder={false}
+            className="custom-class ml-2 mr-2"
+          >
+            Optimization
+          </GradientText>
+
+          <span>Center</span>
+        </div>
+
+        <p className="text-lg text-gray-400 mt-4 sec-title-hero">
+          AILO is Center of Excellence from Telkom University that focused on AI
+          Research and Development
+        </p>
+
         <AnimatedOnScroll>
           <div className="btn-started">Get Started</div>
         </AnimatedOnScroll>
